ebfc390a6180ad605f33d1a1c9a532aab2e5c636
jjohnson0204 <joejohnson0204@gmail.com>
2025-02-10 17:21:26 -0500

    Attribute Accessors

--- Source/Aura/Public/AbilitySystem/AuraAttributeSet.h
+++ Source/Aura/Public/AbilitySystem/AuraAttributeSet.h
@@ -4,7 +4,14 @@
 
 #include "CoreMinimal.h"
 #include "AttributeSet.h"
+#include "AbilitySystemComponent.h"
 #include "AuraAttributeSet.generated.h"
+
+#define ATTRIBUTE_ACCESSORS(ClassName, PropertyName) \
+		GAMEPLAYATTRIBUTE_PROPERTY_GETTER(ClassName, PropertyName) \
+		GAMEPLAYATTRIBUTE_VALUE_GETTER(PropertyName) \
+		GAMEPLAYATTRIBUTE_VALUE_SETTER(PropertyName) \
+		GAMEPLAYATTRIBUTE_VALUE_INITTER(PropertyName)
 
 /**
  * 
@@ -27,57 +34,57 @@ public:
 	//Strength
 	UPROPERTY(BlueprintReadOnly, ReplicatedUsing= OnRep_Strength, Category= "Primary Attributes")
 	FGameplayAttributeData Strength;
-	/*ATTRIBUTE_ACCESSORS(UAuraAttributeSet, Strength);*/
+	ATTRIBUTE_ACCESSORS(UAuraAttributeSet, Strength);
 
 	//Intelligence
 	UPROPERTY(BlueprintReadOnly, ReplicatedUsing= OnRep_Intelligence, Category= "Primary Attributes")
 	FGameplayAttributeData Intelligence;
-	/*ATTRIBUTE_ACCESSORS(UAuraAttributeSet, Intelligence);*/
+	ATTRIBUTE_ACCESSORS(UAuraAttributeSet, Intelligence);
 
 	//Resilience
 	UPROPERTY(BlueprintReadOnly, ReplicatedUsing= OnRep_Resilience, Category= "Primary Attributes")
 	FGameplayAttributeData Resilience;
-	/*ATTRIBUTE_ACCESSORS(UAuraAttributeSet, Resilience);*/
+	ATTRIBUTE_ACCESSORS(UAuraAttributeSet, Resilience);
 
 	//Vigor
 	UPROPERTY(BlueprintReadOnly, ReplicatedUsing= OnRep_Vigor, Category= "Primary Attributes")
 	FGameplayAttributeData Vigor;
-	/*ATTRIBUTE_ACCESSORS(UAuraAttributeSet, Vigor);*/
+	ATTRIBUTE_ACCESSORS(UAuraAttributeSet, Vigor);
 
 	/*
 	 *Secondary Attributes
 	 */
 	UPROPERTY(BlueprintReadOnly, ReplicatedUsing= OnRep_Armor, Category= "Secondary Attributes")
 	FGameplayAttributeData Armor;
-	/*ATTRIBUTE_ACCESSORS(UAuraAttributeSet, Armor);*/
+	ATTRIBUTE_ACCESSORS(UAuraAttributeSet, Armor);
 
 	UPROPERTY(BlueprintReadOnly, ReplicatedUsing= OnRep_ArmorPenetration, Category= "Secondary Attributes")
 	FGameplayAttributeData ArmorPenetration;
-	/*ATTRIBUTE_ACCESSORS(UAuraAttributeSet, ArmorPenetration);*/
+	ATTRIBUTE_ACCESSORS(UAuraAttributeSet, ArmorPenetration);
 
 	UPROPERTY(BlueprintReadOnly, ReplicatedUsing= OnRep_BlockChance, Category= "Secondary Attributes")
 	FGameplayAttributeData BlockChance;
-	/*ATTRIBUTE_ACCESSORS(UAuraAttributeSet, BlockChance);*/
+	ATTRIBUTE_ACCESSORS(UAuraAttributeSet, BlockChance);
 
 	UPROPERTY(BlueprintReadOnly, ReplicatedUsing= OnRep_CriticalHitChance, Category= "Secondary Attributes")
 	FGameplayAttributeData CriticalHitChance;
-	/*ATTRIBUTE_ACCESSORS(UAuraAttributeSet, CriticalHitChance);*/
+	ATTRIBUTE_ACCESSORS(UAuraAttributeSet, CriticalHitChance);
 
 	UPROPERTY(BlueprintReadOnly, ReplicatedUsing= OnRep_CriticalHitDamage, Category= "Secondary Attributes")
 	FGameplayAttributeData CriticalHitDamage;
-	/*ATTRIBUTE_ACCESSORS(UAuraAttributeSet, CriticalHitDamage);*/
+	ATTRIBUTE_ACCESSORS(UAuraAttributeSet, CriticalHitDamage);
 
 	UPROPERTY(BlueprintReadOnly, ReplicatedUsing= OnRep_CriticalHitResistance, Category= "Secondary Attributes")
 	FGameplayAttributeData CriticalHitResistance;
-	/*ATTRIBUTE_ACCESSORS(UAuraAttributeSet, CriticalHitResistance);*/
+	ATTRIBUTE_ACCESSORS(UAuraAttributeSet, CriticalHitResistance);
 
 	UPROPERTY(BlueprintReadOnly, ReplicatedUsing= OnRep_HealthRegeneration, Category= "Secondary Attributes")
 	FGameplayAttributeData HealthRegeneration;
-	/*ATTRIBUTE_ACCESSORS(UAuraAttributeSet, HealthRegeneration);*/
+	ATTRIBUTE_ACCESSORS(UAuraAttributeSet, HealthRegeneration);
 
 	UPROPERTY(BlueprintReadOnly, ReplicatedUsing= OnRep_ManaRegeneration, Category= "Secondary Attributes")
 	FGameplayAttributeData ManaRegeneration;
-	/*ATTRIBUTE_ACCESSORS(UAuraAttributeSet, ManaRegeneration);*/
+	ATTRIBUTE_ACCESSORS(UAuraAttributeSet, ManaRegeneration);
 	
 	/*
 	 *Vital Attributes
@@ -86,18 +93,18 @@ public:
 	// Health
 	UPROPERTY(BlueprintReadOnly, ReplicatedUsing = OnRep_Health, Category = "Vital Attributes")
 	FGameplayAttributeData Health ;
-	/*ATTRIBUTE_ACCESSORS(UAuraAttributeSet, Health);*/
+	ATTRIBUTE_ACCESSORS(UAuraAttributeSet, Health);
 	UPROPERTY(BlueprintReadOnly, ReplicatedUsing = OnRep_MaxHealth, Category = "Vital Attributes")
 	FGameplayAttributeData MaxHealth ;
-	/*ATTRIBUTE_ACCESSORS(UAuraAttributeSet, MaxHealth);*/
+	ATTRIBUTE_ACCESSORS(UAuraAttributeSet, MaxHealth);
 
 	//Mana
 	UPROPERTY(BlueprintReadOnly, ReplicatedUsing = OnRep_Mana, Category = "Vital Attributes")
 	FGameplayAttributeData Mana ;
-	/*ATTRIBUTE_ACCESSORS(UAuraAttributeSet, Mana);*/
+	ATTRIBUTE_ACCESSORS(UAuraAttributeSet, Mana);
 	UPROPERTY(BlueprintReadOnly, ReplicatedUsing = OnRep_MaxMana, Category = "Vital Attributes")
 	FGameplayAttributeData MaxMana ;
-	/*ATTRIBUTE_ACCESSORS(UAuraAttributeSet, MaxMana);*/
+	ATTRIBUTE_ACCESSORS(UAuraAttributeSet, MaxMana);
 
 	/*
 	 *Resistance Attributes
@@ -105,26 +112,26 @@ public:
 	//Fire
 	UPROPERTY(BlueprintReadOnly, ReplicatedUsing = OnRep_ArcaneResistance, Category= "Resistance Attributes")
 	FGameplayAttributeData ArcaneResistance;
-	/*ATTRIBUTE_ACCESSORS(UAuraAttributeSet, ArcaneResistance);*/
+	ATTRIBUTE_ACCESSORS(UAuraAttributeSet, ArcaneResistance);
 
 	UPROPERTY(BlueprintReadOnly, ReplicatedUsing = OnRep_FireResistance, Category= "Resistance Attributes")
 	FGameplayAttributeData FireResistance;
-	/*ATTRIBUTE_ACCESSORS(UAuraAttributeSet, FireResistance);*/
+	ATTRIBUTE_ACCESSORS(UAuraAttributeSet, FireResistance);
 
 	UPROPERTY(BlueprintReadOnly, ReplicatedUsing = OnRep_LightningResistance, Category= "Resistance Attributes")
 	FGameplayAttributeData LightningResistance;
-	/*ATTRIBUTE_ACCESSORS(UAuraAttributeSet, LightningResistance);*/
+	ATTRIBUTE_ACCESSORS(UAuraAttributeSet, LightningResistance);
 
 	UPROPERTY(BlueprintReadOnly, ReplicatedUsing = OnRep_PhysicalResistance, Category= "Resistance Attributes")
 	FGameplayAttributeData PhysicalResistance;
-	/*ATTRIBUTE_ACCESSORS(UAuraAttributeSet, PhysicalResistance);*/
+	ATTRIBUTE_ACCESSORS(UAuraAttributeSet, PhysicalResistance);
 	
 	/*
 	 *Meta Attributes
 	 */
 	UPROPERTY(BlueprintReadOnly, Category="Meta Attributes")
 	FGameplayAttributeData IncomingDamage;
-	/*ATTRIBUTE_ACCESSORS(UAuraAttributeSet, IncomingDamage);*/
+	ATTRIBUTE_ACCESSORS(UAuraAttributeSet, IncomingDamage);
 
 	// Create RepNotify that excepts the old value
 	//Health
