7977ebbbb35332b07831d28e03592dec1fcb3a9a
jjohnson0204 <joejohnson0204@gmail.com>
2025-02-10 17:55:00 -0500

    Aura User Widget and Widget Controller

---
+++ Source/Aura/Private/UI/Widget/AuraUserWidget.cpp
@@ -0,0 +1,10 @@
+// Copyright Light1111
+
+
+#include "UI/Widget/AuraUserWidget.h"
+
+void UAuraUserWidget::SetWidgetController(UObject* InWidgetController)
+{
+	WidgetController = InWidgetController;
+	WidgetControllerSet();
+}
---
+++ Source/Aura/Private/UI/WidgetController/AuraWidgetController.cpp
@@ -0,0 +1,5 @@
+// Copyright Light1111
+
+
+#include "UI/WidgetController/AuraWidgetController.h"
+
