e31f09bc48bd379db94e6b416b4062b556d2fe2b
jjohnson0204 <joejohnson0204@gmail.com>
2025-02-16 10:10:16 -0500

    Aura Asset Manager

---
+++ Source/Aura/Private/AuraAssetManager.cpp
@@ -0,0 +1,22 @@
+// Copyright Light1111
+
+
+#include "AuraAssetManager.h"
+#include "AuraGameplayTags.h"
+
+UAuraAssetManager& UAuraAssetManager::Get()
+{
+	
+	check(GEngine);
+	
+	UAuraAssetManager* AuraAssetManager = Cast<UAuraAssetManager>(GEngine->AssetManager);
+	return *AuraAssetManager;
+}
+
+void UAuraAssetManager::StartInitialLoading()
+{
+	Super::StartInitialLoading();
+	
+	FAuraGameplayTags::InitializeNativeGameplayTags();
+}
+
--- Source/Aura/Public/AuraGameplayTags.h
+++ Source/Aura/Public/AuraGameplayTags.h
@@ -18,6 +18,8 @@ public:
 	
 	static const FAuraGameplayTags& Get() { return GameplayTags;}
 	static void InitializeNativeGameplayTags();
+
+	FGameplayTag Attributes_Secondary_Armor;
 	
 protected:
 	
