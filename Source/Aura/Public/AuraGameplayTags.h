0345d7d08ad9e8d9b1909cca51c1234902d79c3b
jjohnson0204 <joejohnson0204@gmail.com>
2025-02-15 23:09:45 -0500

    Gameplay Tags Singleton

---
+++ Source/Aura/Public/AuraGameplayTags.h
@@ -0,0 +1,28 @@
+// Copyright Light1111
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameplayTagContainer.h"
+
+/**
+ * AuraGameplayTags
+ *
+ * Singleton containing native Gameplay Tags
+ */
+struct FAuraGameplayTags
+
+{
+	
+public:
+	
+	static const FAuraGameplayTags& Get() { return GameplayTags;}
+	static void InitializeNativeGameplayTags();
+	
+protected:
+	
+private:
+	
+	static FAuraGameplayTags GameplayTags;
+	
+};
