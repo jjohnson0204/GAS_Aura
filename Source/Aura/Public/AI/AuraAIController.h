196bc98e6b49e95e22bf4720ec81316be07b5fe4
jjohnson0204 <joejohnson0204@gmail.com>
2025-02-20 19:40:27 -0500

    AI Controller Blackboard and Behavior Tree

---
+++ Source/Aura/Public/AI/AuraAIController.h
@@ -0,0 +1,25 @@
+// Copyright Light1111
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "AIController.h"
+#include "AuraAIController.generated.h"
+
+class UBlackboardComponent;
+class UBehaviorTreeComponent;
+
+/**
+ * 
+ */
+UCLASS()
+class AURA_API AAuraAIController : public AAIController
+{
+	GENERATED_BODY()
+public:
+	AAuraAIController();
+protected:
+
+	UPROPERTY()
+	TObjectPtr<UBehaviorTreeComponent> BehaviorTreeComponent;
+};
