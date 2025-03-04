39907498cea3754fee58220823a8a58fdc4a00af
jjohnson0204 <joejohnson0204@gmail.com>
2025-03-04 13:13:54 -0500

    Gameplay Ability Cooldown

--- Source/Aura/Public/AuraGameplayTags.h
+++ Source/Aura/Public/AuraGameplayTags.h
@@ -73,6 +73,8 @@ public:
 
 	FGameplayTag Abilities_Fire_FireBolt;
 
+	FGameplayTag Cooldown_Fire_FireBolt;
+
 	FGameplayTag CombatSocket_Weapon;
 	FGameplayTag CombatSocket_RightHand;
 	FGameplayTag CombatSocket_LeftHand;
