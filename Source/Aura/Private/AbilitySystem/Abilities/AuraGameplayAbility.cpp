5970140a3bb72f57a181ac4974eaff1c2457f43b
jjohnson0204 <joejohnson0204@gmail.com>
2025-04-01 00:02:06 -0400

    Cost and Cooldown in Spell Description

--- Source/Aura/Private/AbilitySystem/Abilities/AuraGameplayAbility.cpp
+++ Source/Aura/Private/AbilitySystem/Abilities/AuraGameplayAbility.cpp
@@ -3,6 +3,8 @@
 
 #include "AbilitySystem/Abilities/AuraGameplayAbility.h"
 
+#include "AbilitySystem/AuraAttributeSet.h"
+
 FString UAuraGameplayAbility::GetDescription(int32 Level)
 {
 	return FString::Printf(TEXT("<Default>%s, </><Level>%d</>"), L"Default Ability Name - Bacon ipsum dolor amet nisi t-bone qui, salami culpa leberkas shankle biltong boudin filet mignon brisket laboris sunt. In magna t-bone incididunt prosciutto. Non cow culpa cupim. Consequat sed incididunt swine lorem, venison anim meatloaf sausage minim.", Level);
@@ -16,4 +18,31 @@ FString UAuraGameplayAbility::GetNextLevelDescription(int32 Level)
 FString UAuraGameplayAbility::GetLockedDescription(int32 Level)
 {
 	return FString::Printf(TEXT("<Default>Spell Locked Until Level: %d</>"), Level);
+}
+
+float UAuraGameplayAbility::GetManaCost(float InLevel) const
+{
+	float ManaCost = 0.f;
+	if (const UGameplayEffect* CostEffect = GetCostGameplayEffect())
+	{
+		for (FGameplayModifierInfo Mod : CostEffect->Modifiers)
+		{
+			if (Mod.Attribute == UAuraAttributeSet::GetManaAttribute())
+			{
+				Mod.ModifierMagnitude.GetStaticMagnitudeIfPossible(InLevel, ManaCost);
+				break;
+			}
+		}
+	}
+	return ManaCost;
+}
+
+float UAuraGameplayAbility::GetCooldown(float InLevel) const
+{
+	float Cooldown = 0.f;
+	if (const UGameplayEffect* CooldownEffect = GetCooldownGameplayEffect())
+	{
+		CooldownEffect->DurationMagnitude.GetStaticMagnitudeIfPossible(InLevel, Cooldown);
+	}
+	return Cooldown;
 }
