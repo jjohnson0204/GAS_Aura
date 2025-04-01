b7b5c46dd9b8f4ffd67eb33e00a35f337029f6ec
jjohnson0204 <joejohnson0204@gmail.com>
2025-03-31 22:35:35 -0400

    FireBolt Description

--- Source/Aura/Private/AbilitySystem/Abilities/AuraGameplayAbility.cpp
+++ Source/Aura/Private/AbilitySystem/Abilities/AuraGameplayAbility.cpp
@@ -5,7 +5,7 @@
 
 FString UAuraGameplayAbility::GetDescription(int32 Level)
 {
-	return FString::Printf(TEXT("<Default>%s, </><Level>%d</>"), L"Default Ability Name - LoremIpsum LoremIpsum LoremIpsum LoremIpsum LoremIpsum LoremIpsum LoremIpsum LoremIpsum LoremIpsum LoremIpsum LoremIpsum LoremIpsum LoremIpsum LoremIpsum LoremIpsum LoremIpsum", Level);
+	return FString::Printf(TEXT("<Default>%s, </><Level>%d</>"), L"Default Ability Name - Bacon ipsum dolor amet nisi t-bone qui, salami culpa leberkas shankle biltong boudin filet mignon brisket laboris sunt. In magna t-bone incididunt prosciutto. Non cow culpa cupim. Consequat sed incididunt swine lorem, venison anim meatloaf sausage minim.", Level);
 }
 
 FString UAuraGameplayAbility::GetNextLevelDescription(int32 Level)
